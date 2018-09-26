9063129d89cfc0e329707a76b69bb16eefd17b90
Simiyu Nabende <simiyunabende@Simiyus-MacBook-Pro.local>
2018-09-26 17:40:43 +0300

    feat(user-details): launch user details on userlist click
     - create a function to call a new intent in main activity
    
    [Finishes #155820624]

--- app/src/main/java/com/example/simiyunabende/javadevs/MainActivity.java
+++ app/src/main/java/com/example/simiyunabende/javadevs/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.simiyunabende.javadevs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout layout =  findViewById(R.id.user_list_layout);
+        layout.setOnClickListener(this);
+    }
+
+    // Handle onClick action
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, UserDetails.class);
+        startActivity(intent);
+
     }
 }
